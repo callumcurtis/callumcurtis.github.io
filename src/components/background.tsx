b6a45eebb5ee2b9ad42c6bb77518473c3b9ec6be
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-04-06 23:53:03 +0000

    Remove waveOptions from  wrapped component

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import WAVES from 'vanta/dist/vanta.waves.min';
 
 
-const withWaveAnimationBackground = <P extends {waveOptions?: {}}>(Component: React.ComponentType<Omit<P, "waveOptions">>) => (props: P) => {
+const withWaveAnimationBackground = <P extends {waveOptions?: {}}>(Component: React.ComponentType<Omit<P, "waveOptions">>) => ({waveOptions, ...rest}: P) => {
   const [vantaEffect, setVantaEffect] = React.useState<{ destroy: () => void } | null>(null)
   const myRef = React.useRef(null)
   React.useEffect(() => {
     if (!vantaEffect) {
       setVantaEffect(WAVES({
         el: myRef.current,
-        ...props.waveOptions,
+        ...waveOptions,
       }))
     }
     return () => {
       if (vantaEffect) vantaEffect.destroy()
     }
-  }, [vantaEffect, props.waveOptions])
-  return <Component {...props} ref={myRef} />
+  }, [vantaEffect, waveOptions])
+  return <Component {...rest} ref={myRef} />
 }
 
 export { withWaveAnimationBackground }
